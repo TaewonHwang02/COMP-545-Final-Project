8e1b227ce0047fe880d0d7f2c93ad61fe2386efa
Jin Won Lee <jinwonlee0916@gmail.com>
2025-12-13 23:17:03 -0500

    slurm job

---
+++ benchmark.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=mmlu
+#SBATCH --account=ctb-timod 
+#SBATCH --time=32:00:00 
+#SBATCH --mem=64G
+#SBATCH --gres=gpu:a100:1 
+#SBATCH --cpus-per-task=4
+#SBATCH --ntasks=1 
+#SBATCH --output=logs/bagel_%j.out
+#SBATCH --error=logs/bagel_%j.err 
+               
+source venv/bin/activate
+
+python mmlu_bagel.py 2>&1 | tee mmlu_bagel_logs_server.txt
